802c35773608f53c0a48a02d76b9935b01c86b9a
Sam Scott <116088229+ProfSamScott@users.noreply.github.com>
2024-04-04 12:45:10 -0400

    Create doubler.c

---
+++ week3/doubler.c
@@ -0,0 +1,28 @@
+/* Reads a list of integers from stdin, doubles each one, and outputs to stdout.
+ * Writes to stderr and exits if it encounters a bad value.
+ *
+ * Sam Scott, McMaster University , 2024
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+int main()
+{
+    int value;
+    while (true)
+    {
+        int result = scanf("%d", &value);
+        if (result == 0)
+        {
+            fprintf(stderr, "Bad Value\n");
+            return EXIT_FAILURE;
+        }
+        if (result == EOF)
+            break;
+        value = value * 2;
+        printf("%d ", value);
+    }
+    puts("");
+    return EXIT_SUCCESS;
+}
