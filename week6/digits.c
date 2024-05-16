0d0f307d6038a47e24eac9699485f3d503684efa
Sam Scott <samscott@mcmaster.ca>
2024-05-16 11:51:37 -0400

    commenting for digits.c

--- week6/digits.c
+++ week6/digits.c
@@ -1,6 +1,15 @@
+/* Starter code for Lab 6.1 Activity
+ *
+ * This file will not compile as is. You need to create the digits function.
+ *
+ */
 #include <stdio.h>
 #include <stdlib.h>
 
+/* Main function. Runs test cases for the digits function.
+ * Exits with failure code on first test case fail.
+ * Otherwise exits with success.
+ */
 int main() {
     int min, max, sum;
     long n;
@@ -14,4 +23,6 @@ int main() {
         puts("failure");
         return EXIT_FAILURE;
     }
+
+    return EXIT_SUCCESS;
 }
