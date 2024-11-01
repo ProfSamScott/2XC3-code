9e565534d1abff7cf199aef257af9a9e3e7ca2cf
Sam Scott <samscott@mcmaster.ca>
2024-11-01 13:13:48 -0400

    commented primes.c

--- week09/primes.c
+++ week09/primes.c
@@ -1,3 +1,5 @@
+/* This file is to be used with Lab 9.2. Commenting is minimal
+ * so you can focus on the code. Sam Scott, McMaster, 2024.*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
@@ -7,10 +9,12 @@ bool quickcheck(int n) {
     return n < 2;
 }
 
+/* return true if b is a factor of a, false otherwise */
 bool checkfactor(int a, int b) {
     return a % b == 0;
 }
 
+/* return true if n is prime, false otherwise */
 bool isprime(int n) {
     if (quickcheck(n)) 
         return false;
@@ -22,11 +26,12 @@ bool isprime(int n) {
     return true;
 }
 
+/* print primes from -200000 to 200000 */
 void main() {
-    long start = clock();
+    long start = clock(); // start timer
     for (int n=-200000; n<=200000; n++)
         if (isprime(n)) 
             printf("%d ",n);
-    long end = clock();
+    long end = clock();   // end timer
     printf("\nRunning time: %.4f seconds\n",(float)(end-start)/1000000);
 }
