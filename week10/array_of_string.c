20b7b3007b1f9ba2d459fa39b43989cda7d4ca74
Sam Scott <samscott@mcmaster.ca>
2024-11-12 09:58:46 -0500

    Week 10 changes

--- week10/array_of_string.c
+++ week10/array_of_string.c
@@ -15,27 +15,37 @@ void main(int argc, char argv[][100]) {
     char words[N][LENGTH]={"word", "what", "where", "why"}; // rectangular: wasteful 
     //char *words[] = {"word", "what", "where", "why"};     // ragged: immutable
 
+    puts("Printing array of strings context");
     for (int i=0; i<N; i++) 
         printf("%s ",words[i]);
     puts("");
 
+    puts("Changing first character of each string");
     for (int i=0; i<N; i++)  // Segmentation fault if 'words' is immutable
         words[i][0] = 'W';
 
+    puts("Printing new contents");
     for (int i=0; i<N; i++)
         printf("%s ",words[i]);
     puts("");
 
+    puts("Getting 10 strings from the user");
     char *a[10];
     char inp[1000];
     for (int i=0; i<10; i++) {
-        scanf("%s", inp);
+        scanf("%999s", inp);
         a[i] = malloc(strlen(inp)+1);
         strcpy(a[i], inp);
     }
+	puts("");
+
+	puts("Printing the results");
     for (int i=0; i<10; i++) {
         printf("%s\n", a[i]);
     }
+	puts("");
+
+	puts("Deallocating memory");
     for (int i=0; i<10; i++) {
         free(a[i]);
     }
--- week10/string.c
+++ week10/string.c
@@ -14,10 +14,12 @@ void main() {
     printf("strings: %10s %10s %10s\n", s, t, u);
     printf("sizeof:  %10lu %10lu %10lu\n", sizeof(s), sizeof(t), sizeof(u));
     printf("strlen:  %10lu %10lu %10lu\n", strlen(s), strlen(t), strlen(u));
+    printf("\\0: %d\n",'\0');
 
     s[0] = 'H';
     //t[0] = 'G'; // segmentation fault (t is immutable)
     u[0] = 'H';
+	//u[strlen(u)] = '!'; // what will happen?
 
     printf("strings: %10s %10s %10s\n", s, t, u);
 }
