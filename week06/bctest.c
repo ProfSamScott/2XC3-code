6526876c85871c31b127ba6fce5c122025048cf7
Sam Scott <samscott@mcmaster.ca>
2025-09-23 12:41:56 -0400

    added bctest.c to week06

---
+++ week06/bctest.c
@@ -0,0 +1,15 @@
+/* This is a stub program. You should remove this, but you can compile it 
+ * and then try test_script with it. All the test cases will fail.
+ *
+ * if you add return EXIT_FAILURE; then the test cases will fail for a
+ * different reason.
+ *
+ * Sam Scott, McMaster University, 2025 
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+	puts("This is just a test!");
+}
